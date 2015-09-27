ccefd386878e7520baa4f2081c435f4892d363c5
Santiago-vdk <Santiago.vdk@gmail.com>
2015-09-27 06:05:29 -0600

    Termino implementaciones visuales, logicas y adaptacion con Bluemix.

--- src/java/urSQL/servicios/WebAppResource.java
+++ src/java/urSQL/servicios/WebAppResource.java
@@ -20,6 +20,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import urSQL.logica.logHandler;
@@ -52,10 +53,10 @@ public class WebAppResource {
      * @throws InterruptedException
      * @throws IOException
      */
-    @POST
+    @POST 
     @Path("/ExecuteQuery")
     @Produces("application/json")
-    @Consumes("application/json")
+    @Consumes("application/json") 
     public String postQuery(String msg) throws JSONException, InterruptedException, IOException {
 
         /* No borrar
@@ -69,12 +70,43 @@ public class WebAppResource {
          String path = ctx.getRealPath("/");
          logHandler.getInstance().logEvent(path, false, msg, "executed", String.valueOf(duration));
          */
-        JSONObject obj = new JSONObject();
-        obj.put("dmata", "daca");
+        JSONArray matrix = new JSONArray();
 
-        Thread.sleep(1000);
+        JSONArray fila1 = new JSONArray();
+        fila1.put("1");
+        fila1.put("2");
+        fila1.put("3");
+        
+        
+        JSONArray fila2 = new JSONArray();
+        fila2.put("4");
+        fila2.put("5");
+        fila2.put("6");
+        
+        JSONArray fila3 = new JSONArray();
+        fila3.put("7");
+        fila3.put("8");
+        fila3.put("9");
+        
+        
+        matrix.put(fila1);
+        matrix.put(fila2);
+        matrix.put(fila3);
+        
+        JSONArray col_names = new JSONArray();
+        
+        col_names.put("col1");
+        col_names.put("col2");
+        col_names.put("col3");
+        
+        JSONObject master = new JSONObject();
+        
+        master.put("Valores", matrix);
+        master.put("Columnas", col_names);
+        
 
-        return obj.toString();
+
+        return master.toString();
     }
 
     /**
@@ -89,28 +121,129 @@ public class WebAppResource {
     @Path("/data/log")
     @Produces("application/json")
     public String queryLog(String msg) throws JSONException, IOException, NamingException {
-        Object serverName = new InitialContext().lookup("serverName");
+        // Object serverName = new InitialContext().lookup("serverName");
 
-        // String path = ctx.getRealPath("/");
-        String JSONResponse = logHandler.getInstance().getLastEventJSON(serverName.toString());
-        //logHandler.getInstance().logExecution_Plan("asdfasdfasdfasdfa");
-        //logHandler.getInstance().getExecutionPlan();
+        String path = ctx.getRealPath("/");
+        String JSONResponse = logHandler.getInstance().getLastEventJSON("not-used");
+
         return JSONResponse;
+
+        /* logHandler.getInstance().logEvent("dp", false, "asdf", "executed", "Test");
+        
+         logHandler.getInstance().logExecution_Plan("asdfasdfasdf\nasdfa");*/
     }
-    
+
+    @GET
+    @Path("/data/executionPlan")
+    @Produces("application/json")
+    public String getExecPlan(String msg) throws JSONException, IOException, NamingException {
+        // Object serverName = new InitialContext().lookup("serverName");
+
+        String plan = logHandler.getInstance().getExecutionPlan();
+
+        return plan;
+
+        /* logHandler.getInstance().logEvent("dp", false, "asdf", "executed", "Test");
+        
+         logHandler.getInstance().logExecution_Plan("asdfasdfasdf\nasdfa");*/
+    }
+
     @GET
     @Path("/initialSetup")
     @Produces("application/json")
-    public String contextChecker(){
+    public String contextChecker() {
         try {
             String realPath = ctx.getRealPath("/");
             realPath = realPath.substring(0, realPath.indexOf("/apps"));
             logHandler.getInstance().verifyStructure(realPath);
-            
+            logHandler.getInstance().logEvent("not-used", false, "START SERVER", "executed", "-");
+
+            //Pongo datos en el log para probar
+            //
+            //Pruebo el plan
+            logHandler.getInstance().logExecution_Plan("asdfasdfasdf\nasdfa");
+
         } catch (Exception e) {
-            return "Error generating urSQL Folder Context.";
+            return "Error generating urSQL Folder Structure.";
         }
-        
+
         return "true";
-    } 
+    }
+
+    @GET
+    @Path("/data/status")
+    @Produces("application/json")
+    public String getStatus() throws JSONException {
+        JSONObject obj = new JSONObject();
+
+        obj.put("RuntimeDBProcessor", "Running...");
+        obj.put("SystemCatalog", "Running...");
+        obj.put("StoredDataManager", "Running...");
+        obj.put("StoredData", "Running...");
+
+        return obj.toString();
+    }
+
+    @GET
+    @Path("/data/dataBases")
+    @Produces("application/json")
+    public String listDataBases() throws JSONException {
+        JSONObject obj = new JSONObject();
+        JSONArray dbs = new JSONArray();
+
+        dbs.put("db1");
+        dbs.put("db2");
+        dbs.put("db3");
+
+        obj.put("schemas", dbs);
+
+        return obj.toString();
+    }
+
+    @GET
+    @Path("/data/refreshSchemaTree")
+    @Produces("application/json")
+    public String getCurrentSchemaTree() throws JSONException {
+        JSONArray globalarray = new JSONArray();
+
+        JSONArray container = new JSONArray();
+
+        JSONObject tables = new JSONObject();
+        JSONArray tablasdedb1 = new JSONArray();
+
+        tablasdedb1.put("Estudiantes");
+        tablasdedb1.put("Profesores");
+
+        tables.put("tables", tablasdedb1);
+
+        JSONObject name = new JSONObject();
+        name.put("name", "db1");
+
+        container.put(name);
+        container.put(tables);
+
+        globalarray.put(container);
+
+        /////////////////////////////////////////////
+        JSONArray container2 = new JSONArray();
+
+        JSONObject tables2 = new JSONObject();
+        JSONArray tablasdedb2 = new JSONArray();
+
+        tablasdedb2.put("Estudiantes2");
+        tablasdedb2.put("Profesores2");
+
+        tables2.put("tables", tablasdedb2);
+
+        JSONObject name2 = new JSONObject();
+        name2.put("name", "db2");
+
+        container2.put(name2);
+        container2.put(tables2);
+
+        globalarray.put(container2);
+
+        System.out.println(globalarray.toString());
+        return globalarray.toString();
+    }
 }
--- web/js/webapp/webapp-tree.js
+++ web/js/webapp/webapp-tree.js
@@ -17,13 +17,49 @@ $(document).ready(function () {
 
 });
 
-function refreshTree(){
-    console.log("refresh");
+function refreshTree() {
+    $('#jstree').jstree().delete_node();
+    $.ajax({
+        url: 'webresources/webapp/data/refreshSchemaTree',
+        type: 'get',
+        contentType: 'application/json',
+        dataType: "json",
+        success: function (data, textStatus, jQxhr) {
+            console.log("Tree refreshed!");
+           // alert(data[0][1].tables[0]); //Nombre de tabla
+            //alert(data[0][1].tables.length); //tama;o abla interna
+            
+             for(var i = 0; i < data.length; i ++){
+               addDBNode(data[i][0].name);
+                for(var j =0; j < data[0][1].tables.length; j++){
+                    addTableNode(data[i][1].tables[j], data[i][0].name);
+                    
+                }
+            }
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+    });
 }
 
-function addNode(name) {
+function addTableNode(name, parent){    
+    
+    $("#jstree").jstree(true).delete_node(name + "_node");
+    
+    var nodeID = $("#jstree").jstree(true).create_node(parent + "_node", name);
+    $('#jstree').jstree(true).set_id(nodeID, name + "_node");
+    
+    
+}
+
+function addDBNode(name) {
+    
+     $("#jstree").jstree(true).delete_node(name + "_node");
 
     var nodeID = $("#jstree").jstree(true).create_node($("#root")[0], name);
     $('#jstree').jstree(true).set_id(nodeID, name + "_node");
- 
+    
+
+
 }
