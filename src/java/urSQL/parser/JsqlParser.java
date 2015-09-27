8e8e9e6b8258480df8e2099248ac60e39860c025
Rafael Chacón Fallas <rafangel1228@gmail.com>
2015-09-26 19:06:35 -0600

    cambio menor

--- src/java/urSQL/parser/JsqlParser.java
+++ src/java/urSQL/parser/JsqlParser.java
@@ -8,7 +8,6 @@ package urSQL.parser;
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import urSQL.logica.ExecutionPlan;
- 
 /**
  *
  * @author RafaelAngel
@@ -32,26 +31,71 @@ public class JsqlParser {
             return true;
         }
         catch (JSQLParserException ex) {
+            SecondTry(pQuerry);
             String msj = ex.getCause().toString();
-            System.out.println(msj);
-            //System.out.println(msj.substring(30, msj.indexOf("Was expecting")));
+            //System.out.println(msj);
+            System.out.println(msj.substring(30, msj.indexOf("Was expecting")));
             return false;
+           
+           
+            //ver si es un clp o alter
+           
            
             /*
             Error: 1149 SQLSTATE: 42000 (ER_SYNTAX_ERROR)
  
 Message: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use
            
-           
-           
-           
-           
-            "DEleTE\n" +
-                "\n" +
-                "FROM\n" +
-                "WHERE                  Country='Germany'\n" +
-                "AND (     City='Berlin' or City='München');"
             */
+        }
+    }
+   
+    private String elimSpaces(String pQuery){
+        String tmp = "";
+        boolean espacio=true;
+        for(int i=0;i<pQuery.length();i++){
+            if((pQuery.substring(i,i+1).compareTo(" ")==0 && espacio)
+                    || pQuery.substring(i,i+1).compareTo(";")==0
+                    || pQuery.substring(i,i+1).compareTo("\n")==0){
+                //pass
+            }
+            else{
+                if(pQuery.substring(i,i+1).compareTo(" ")==0){
+                    tmp+=pQuery.substring(i,i+1);
+                    espacio = true;
+                }
+                else{
+                    tmp+=pQuery.substring(i,i+1);
+                    espacio = false;
+                }
+            }
+        }
+        return tmp;
+    }
+   
+    private void SecondTry(String pQuery){
+        boolean valido=false;
+        String tmp = elimSpaces(pQuery);
+        if(tmp.substring(0,16).toUpperCase().compareTo("CREATE DATABASE ")==0){
+            valido=true;
+        }
+        if(tmp.toUpperCase().compareTo("LIST DATABASES")==0){
+            valido=true;
+        }
+        if(tmp.toUpperCase().compareTo("START")==0){
+            valido=true;
+        }
+        if(tmp.toUpperCase().compareTo("GET STATUS")==0){
+            valido=true;
+        }
+        if(tmp.toUpperCase().compareTo("STOP")==0){
+            valido=true;
+        }
+        if(tmp.substring(0,17).toUpperCase().compareTo("DISPLAY DATABASE ")==0){
+            valido=true;
+        }
+        if(valido){
+            executionPlan.createPlan(tmp);
         }
     }
    
@@ -61,31 +105,25 @@ Message: You have an error in your SQL syntax; check the manual that corresponds
      */
     public static void main(String [] args)
         {
+           
+           
             JsqlParser parser = new JsqlParser();
-            parser.Parse("CREATE TABLE Persons\n" +
-            "(\n" +
-            "P_Id int NOT NULL,\n" +
-            "LastName varchar(255) NOT NULL,\n" +
-            "FirstName varchar,\n" +
-            "Address DATETIME,\n" +
-            "City varchar(255),\n" +
-            "PRIMARY KEY (P_Id)\n" +
-            ")");
-        
+            parser.Parse("select");
+       
         /*("ALTER TABLE Orders\n" +
-            "ADD CONSTRAINT fk_PerOrders\n" +
+            "ADD CONSTRAINT\n" +
             "FOREIGN KEY (P_Id)\n" +
             "REFERENCES Persons(P_Id)");
-        
+       
         ("DROP TABLE tabla");
-        
-        
+       
+       
         ("CREATE INDEX INDICE ON\n" +
             "\n" +
             "tabla (col1)");
-        
-        
-        
+       
+       
+       
         ("CREATE TABLE Persons\n" +
             "(\n" +
             "P_Id int NOT NULL,\n" +
@@ -95,25 +133,25 @@ Message: You have an error in your SQL syntax; check the manual that corresponds
             "City varchar(255),\n" +
             "PRIMARY KEY (P_Id)\n" +
             ")");
-        
+       
         ("INSERT INTO tabla1 (col2,col1, col5)\n" +
             "\n" +
             "VALUES(1,2,th)");
-        
-        
-        ("UPDATE tabla1\n" + 
+       
+       
+        ("UPDATE tabla1\n" +
             "SET colum = val , col2 = val2\n" +
             "\n" +
             "WHERE colum='Germany';\n");
-        
+       
         ("CREATE DATABASE adname");
-
+ 
         ("SELECT column_name  ,column_name \n FROM table_name;");
                    
                     ("DEleTE\n" +
                 "\n" +
                 "FROM t \n" +
                 "WHERE                  Country='Germany'\n" +
-                "AND (City='Berlin' or City='München');");*/
+                "AND (City='Berlin' or City='MÃ¼nchen');");*/
         }
 }
